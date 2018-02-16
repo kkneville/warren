df6eb60eace090767c2eb8961600dcef4750dccc
Katherine Krueger <Katherine.Krueger@benefitmall.com>
2018-02-16 08:01:02 -0600

    initial commit

---
+++ warren.py
@@ -0,0 +1,30 @@
+# a game of bunnies
+
+import random
+
+class Warren(object):
+	def __init__(self, name, residence, chief):
+		self.name = name
+		self.residence = residence
+		self.chief = chief
+		self.capacity = 100
+		self.owsla = []
+		self.rabbits = []
+		self.log = []
+
+	def count(self):
+		count = 0
+		for rabbit in rabbits:
+			count += 1
+		for member in owsla:
+			count += 1
+		return count
+	
+	def log(self, type, num=0, name1="", name2="")
+		if type = "birth":
+			log.push("{} kits were born to {} and {}".format(num, name1, name2))
+
+
+	
+
+
